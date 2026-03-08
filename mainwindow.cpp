710d102e102feaa64f4b99397a3dc36073445048
lwm <2997504841@qq.com>
2026-03-08 23:04:14 +0800

    实现拖拽放入文件事件，获取文件信息

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,49 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setAcceptDrops(true);// 开启对整个窗口的拖放操作的支持
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent *event)
+{
+    if (event->mimeData()->hasUrls())
+    {
+        event->acceptProposedAction(); // 接受默认的拖放行为
+    }
+}
+
+void MainWindow::dropEvent(QDropEvent *event)
+{
+    QList<QUrl> urls = event->mimeData()->urls();
+
+    // 确保文件数量仅为一个
+    if (urls.size() != 1) {
+        if (urls.size() > 1) {
+            // 报错
+            QMessageBox::warning(this, "warning", "请拖入单个文件");
+        }
+        return;
+    }
+
+    const QUrl& url = urls.first();
+    QString filePath = url.toLocalFile();
+
+    // 检查文件是否存在
+    QFileInfo fileInfo(filePath);
+    if (!fileInfo.exists()) {
+        QMessageBox::warning(this, "warning", "请拖入有效的文件");
+        return;
+    }
+
+    //获取文件信息，并显示到标题栏上
+    m_fileInfo = fileInfo;
+    setWindowTitle(m_fileInfo.absoluteFilePath());
+
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,11 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QFileInfo>
 #include <QMainWindow>
-
+#include <QMessageBox>
+#include <QDragEnterEvent>
+#include <QMimeData>
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
@@ -17,7 +20,12 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dropEvent(QDropEvent *event);
 private:
     Ui::MainWindow *ui;
+
+    QFileInfo m_fileInfo;
 };
 #endif // MAINWINDOW_H
